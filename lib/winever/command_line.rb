d9be301f840fc0c5450facbbaa3162a5f50d0447
Maxime Handfield Lapointe <hunter_spawn@hotmail.com>
2014-08-11 15:45:02 -0400

    Moving command line handling code out of the bin and into the lib's file for CommandLine.

--- lib/winever/command_line.rb
+++ lib/winever/command_line.rb
@@ -1,5 +1,36 @@
 module Winever
   class CommandLine
+
+    def self.run_from_command_line_options
+      require 'optparse'
+
+      options = {}
+
+      OptionParser.new do |opts|
+        opts.banner = "Usage: whenever [options]"
+        opts.on('-i', '--update [identifier]', 'Default: full path to schedule.rb file') do |identifier|
+          options[:update] = true
+          options[:identifier] = identifier if identifier
+        end
+        opts.on('-c', '--clear [identifier]') do |identifier|
+          options[:clear] = true
+          options[:identifier] = identifier if identifier
+        end
+        opts.on('-s', '--set [variables]', 'Example: --set \'environment=staging&path=/my/sweet/path\'') do |set|
+          options[:set] = set if set
+        end
+        opts.on('-f', '--load-file [schedule file]', 'Default: config/schedule.rb') do |file|
+          options[:file] = file if file
+        end
+        opts.on('-k', '--cut [lines]', 'Cut lines from the top of the cronfile') do |lines|
+          options[:cut] = lines.to_i if lines
+        end
+        opts.on('-v', '--version') { puts "Winever v#{Winever::VERSION}"; exit(0) }
+      end.parse!
+
+      self.execute(options)
+    end
+
     def self.execute options={}
       new(options).run
     end
